326a9c7c2d790ceb7f416462e6df99c323862206
Ken Allan <ken@norganna.com>
2015-11-30 21:04:22 +1000

    Initial Commit

---
+++ ColorDial/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ColorDial
+//
+//  Created by Kenneth Allan on 30/11/2015.
+//  Copyright © 2015 Kenneth Allan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ColorDial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ColorDial
+//
+//  Created by Kenneth Allan on 30/11/2015.
+//  Copyright © 2015 Kenneth Allan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
